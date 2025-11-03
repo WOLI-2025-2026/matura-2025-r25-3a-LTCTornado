98fb12fc97bf443525a79603aa229534b30c8bf3
LTCTornado <160650534+LTCTornado@users.noreply.github.com>
2025-11-03 15:15:47 +0000

    a

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
 # Tomasz Dobrowolski
+file = open("/workspaces/matura-2025-r25-3a-LTCTornado/zalaczniki-2025/symbole.txt", "r")  # "r" = read mode
+lines = [line.strip() for line in file.readlines()]
+for line in lines:
+    if line == line[::-1]:
+        print(line)
+    
+
+
+file.close()
