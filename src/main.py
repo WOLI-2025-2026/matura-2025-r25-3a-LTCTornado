2edd8c1b0d37592755fd26de5b8d17adcf6e70b2
LTCTornado <160650534+LTCTornado@users.noreply.github.com>
2025-11-03 15:21:09 +0000

    niggaAI

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 # Tomasz Dobrowolski
+
 file = open("/workspaces/matura-2025-r25-3a-LTCTornado/zalaczniki-2025/symbole.txt", "r")  # "r" = read mode
 lines = [line.strip() for line in file.readlines()]
-for line in lines:
-    if line == line[::-1]:
-        print(line)
-    
+
+def czy_palindrom(lines):
+    for line in lines:
+        if line == line[::-1]:
+            print(line)
+czy_palindrom(lines)    
 
 
 file.close()
